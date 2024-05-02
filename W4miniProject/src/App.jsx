67aec52c4d45d4dec278da62cd067bb1a3e8c16a
Francisco Soares <franciscosoares13@hotmail.com>
2024-05-02 14:13:59 +0100

    time 1413

--- W4miniProject/src/App.jsx
+++ W4miniProject/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import logo from './assets/images/logo.png'
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
     <>
       <div>
+        <Navbar />
         <a href="https://react.dev" target="_blank">
           <img src={logo} className="logo react" alt="React logo" />
         </a>
