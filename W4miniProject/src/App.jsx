0035bc8203345395d412a153b417696a7fadb206
Manuele Vancin <mfvancin@gmail.com>
2024-05-02 14:54:36 +0100

    app changes

--- W4miniProject/src/App.jsx
+++ W4miniProject/src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
-import './App.css'
-import Navbar from './components/Navbar'
+import { useState } from "react";
+import logo from "./assets/images/logo.png";
+import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
-
   return (
-    
+    <div>
       <div>
         <Navbar />
         <a href="https://react.dev" target="_blank">
@@ -13,7 +13,9 @@ function App() {
         </a>
       </div>
 
+      <h1>e-commerce</h1>
+    </div>
   );
 }
 
-export default App
+export default App;
