5b564d5af50f1eb309e3683e8cb75ca7fa4e44db
Manuele Vancin <mfvancin@gmail.com>
2024-05-02 14:29:26 +0100

    AppChanges

--- W4miniProject/src/App.jsx
+++ W4miniProject/src/App.jsx
@@ -1,21 +1,11 @@
 import { useState } from 'react'
-import logo from './assets/images/logo.png'
 import './App.css'
 
 function App() {
 
   return (
-    <>
-      <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={logo} className="logo react" alt="React logo" />
-        </a>
-      </div>
 
-      <h1>e-commerce</h1>
-
-    </>
-  )
+  );
 }
 
 export default App
